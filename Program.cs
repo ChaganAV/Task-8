59c0315d7797268661bfc8118cb962dfdb17d03f
Alexandr <jwchaganav@gmail.com>
2022-10-14 16:37:08 +0400

    задача 8

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число больше 0: ");
+string? sNum = Console.ReadLine();
+
+int num = Convert.ToInt32(sNum);
+int min = 1;
+while(min <= num)
+{
+    if (min%2 == 0)
+        Console.WriteLine(min);
+
+    min++;
+}
+    
